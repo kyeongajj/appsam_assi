22a41217d76079f1d6333c0e68211089688e8752
jkaok <jkaok@gmail.com>
2024-03-16 01:56:41 +0900

    과제제출

--- withyou.js
+++ withyou.js
@@ -27,36 +27,53 @@ $(()=>{
     duration:3
   });
 
-  // let tl = scrollTrigger.create({
-  //     trigger: ".header",
-  //     // pin: true, // true이면 스크롤이 트리거를 지나갈 때까지 트리거를 고정
-  //     start: "top top", // 트리거가 화면 중간에 닿으면 애니메이션 시작
-  //     // markers: true,
-  //     //end: "+=200", // 2000px만큼 스크롤하면 애니메이션 끝
-  //     scrub: 1.5, // 스크롤 속도에 따라 애니메이션 속도가 달라짐
-  // });
-  // tl.to(".header", {y:"-50%"});
-
-
   let textani = gsap.timeline({
     scrollTrigger:{
-      trigger:".content",
+      trigger:".content01",
       start:"20% 30%",
-      // end:"bottom 80%",
       pin:true,
       endTrigger: ".text_u",
-      end:"+=1400px",
+      end:"+=1000px",
       scrub:1,
       // markers:true
     }
   });
-  textani.to(".text01",{y:-30, autoAlpha:0, duration:0.3})
-  .to(".text_with", {"top":"22%", duration: 1})
+  textani.to(".text_top",{y:-30, autoAlpha:0, duration:0.5})
+  .to(".sticky_box", {y:-30, autoAlpha:0, duration:1})
+  .to(".text_with", {"top":"20%", duration: 1})
   .to(".text_u", { autoAlpha:1, duration:1.5}, "<")
   .to(".text_you", {"bottom" : "71%", autoAlpha:1, duration:1}, "<")
-  .to(".text_with", {"left": "40%", duration: 0.7})
-  .to(".text_you", {"right": "30%", autoAlpha:0, duration:0.7}, "<")
-  .to("text_with", {deley:3, duration:3})
+  .to(".text_with", {"left": "37%", duration: 1.2})
+  .to(".text_you", {"right": "30%", autoAlpha:0, duration:1.2}, "<")
+  
+  .to("text_with", {deley:1, duration:2})
+  
+
+
+  let textani02 = gsap.timeline({
+    scrollTrigger:{
+      trigger: ".container .content02",
+      start: "top 30%",
+      pin: true,
+      scrub: 1,
+      end: "+=2000px",
+      endTrigger: ".cont2 .text03",
+      // markers: true,
+      toggleClass: "toline",
+      onLeave:()=>{
+        let addname = document.querySelector(".content02");
+        addname.classList.add("toline");
+      },
+    }
+  });
+
+  textani02
+  .to(".content02 .cont1", {"transform":"translateY(0%)", autoAlpha: 1, duration: 2})
+  .to(".content02 .cont2 .text02", {"transform":"translateY(0%)", autoAlpha: 1, duration: 2})
+  .to(".content02 .cont2 .line_efect", { autoAlpha: 1, duration: 2})
+  .to(".content02 .cont2 .text_box", {"transform":"translateY(0%)", autoAlpha: 1, duration: 2})
+  .to(".content02 .cont2 .text_box", { deley: 10, duration: 10})
+
 
   
 })//readyfn
